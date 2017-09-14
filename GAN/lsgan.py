2f09745d4de0c3c6a98658d7b85787c5654ff2c1
Groo18t <chenkefan950518@hotmail.com>
2017-09-14 02:13:21 -0400

    add GAN models

--- GAN/lsgan.py
+++ GAN/lsgan.py
@@ -2,14 +2,16 @@
 # 2. Use L2 loss instead of log loss
 
 import os 
-import numpy as np
 import torch
 import torch.nn as nn 
 import torch.optim as optim
 from torch.nn.init import xavier_normal
 from torch.autograd import Variable 
-from torchvision import datasets, transforms
-from PIL import Image
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from tensorflow.examples.tutorials.mnist import input_data
 
 class Generator(nn.Module):
 	def __init__(self, z_dim, h_dim, x_dim):
@@ -45,7 +47,7 @@ def init_weights(layer):
 	if isinstance(layer, nn.Linear):
 		xavier_normal(layer.weight.data)
 
-z_dim = 100
+z_dim = 10
 x_dim = 28*28
 h_dim = 128
 
@@ -57,42 +59,25 @@ def build_graph(gpu = False):
 	G.apply(init_weights)
 	D.apply(init_weights)
 
-	loss = nn.MSELoss()
+	loss = nn.BCELoss()
 
 	if gpu:
 		G.cuda()
 		D.cuda()
 		loss.cuda()
 
-	optimizerG = optim.Adam(G.parameters(), lr = 1e-4)
-	optimizerD = optim.Adam(D.parameters(), lr = 1e-4)
+	optimizerG = optim.Adam(G.parameters(), lr = 1e-3)
+	optimizerD = optim.Adam(D.parameters(), lr = 1e-3)
 
 	return G, D, loss, optimizerG, optimizerD
 
-def train(n_epoch, batch_size, gpu = False, save_path = './lsgan/'):
-	# check path
-	if not os.path.exists(save_path+'G/'):
-		os.makedirs(save_path+'G/')
-	if not os.path.exists(save_path+'D/'):
-		os.makedirs(save_path+'D/')
-	if not os.path.exists(save_path+'test/'):
-		os.makedirs(save_path+'test/')
+def train(n_step = 10000, batch_size = 64, gpu = False):
 
 	# load input dataset
-	mnist = datasets.MNIST('mnist/', train = True, download = True, 
-							transform = transforms.Compose([
-								transforms.ToTensor(), 
-								transforms.Normalize((0.1307,), (0.3801,))]))
-
-	train_loader = torch.utils.data.DataLoader(mnist, 
-												batch_size = batch_size, 
-												shuffle = True, num_workers = 4, 
-												pin_memory = gpu, 
-												drop_last = True)
+	mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 	# build model
 	G, D, loss, optimizerG, optimizerD = build_graph(gpu = gpu)
-
 
 	# testing input
 	test_noise = torch.randn(1, z_dim)
@@ -102,53 +87,80 @@ def train(n_epoch, batch_size, gpu = False, save_path = './lsgan/'):
 	
 	ones_label = Variable(torch.ones(batch_size, 1))
 	zeros_label = Variable(torch.zeros(batch_size, 1))
+	if gpu:
+		ones_label=ones_label.cuda()
+		zeros_label=zeros_label.cuda()
+	count = 0
 
-	for epoch_idx in range(n_epoch):
-		for batch_idx, (data, _) in enumerate(train_loader):
-			noise = torch.randn(batch_size, z_dim)
-			data.resize_(batch_size, x_dim)
-			if gpu:
-				noise.cuda()
-				data.cuda()
-			z = Variable(noise)
-			x = Variable(data)
-			g_sample = G(z)
-			real = D(x)
-			fake = D(g_sample.detach())
+	for step_idx in range(n_step):
+		data, _ = mnist.train.next_batch(batch_size)
+		data = torch.from_numpy(data)
+		noise = torch.randn(batch_size, z_dim)
+		if gpu:
+			noise = noise.cuda()
+			data = data.cuda()
+		z = Variable(noise)
+		x = Variable(data)
+		g_sample = G(z)
+		real = D(x)
+		fake = D(g_sample.detach())
 
-			# When b-c = 1 and b-a = 2, the loss is tantamount to Pearson x^2 divergence.
-			# train the discriminator --minimize L(D) = 0.5*[(D(x)-a)^2+(D(G(z))-b)^2]
-			a = -1
-			b = 1
-			c = 0
-			D.zero_grad()
-			D_loss = 0.5*(torch.mean((real-a)**2)+torch.mean((fake-b)**2))
-			D_loss.backward()
-			optimizerD.step()
+		# When b-c = 1 and b-a = 2, the loss is tantamount to Pearson x^2 divergence.
+		# train the discriminator --minimize L(D) = 0.5*[(D(x)-a)^2+(D(G(z))-b)^2]
+		a = -1
+		b = 1
+		c = 0
+		D.zero_grad()
+		D_loss = 0.5*(torch.mean((real-a)**2)+torch.mean((fake-b)**2))
+		D_loss.backward()
+		optimizerD.step()
 
-			# train the generator -- minimize L(G) = 0.5*(D(G(z))-c)^2
-			G.zero_grad()
-			out = D(g_sample)
-			G_loss = 0.5*torch.mean((out-c)**2)
-			G_loss.backward()
-			optimizerG.step()
-
-			print "Epoch: \033[95m%d\033[0m batch: \033[95m%d\033[0m" % (epoch_idx, batch_idx)
+		noise = torch.randn(batch_size, z_dim)
+		if gpu:
+			noise = noise.cuda()
+		z = Variable(noise)
+		g_sample = G(z)
+		# train the generator -- minimize L(G) = 0.5*(D(G(z))-c)^2
+		G.zero_grad()
+		out = D(g_sample)
+		G_loss = 0.5*torch.mean((out-c)**2)
+		G_loss.backward()
+		optimizerG.step()
 
 		# save the model weights and the testing sample
-		if epoch_idx%1 == 0:
-			torch.save(G.state_dict(), save_path+'G/modelG_%d.pth'%epoch_idx)
-			torch.save(D.state_dict(), save_path+'D/modelD_%d.pth'%epoch_idx)
+		if step_idx % 1000 == 0:
+			print 'Iter-{}; D_loss: {:.4}; G_loss: {:.4}'.format(step_idx, D_loss.data[0], G_loss.data[0])
+			samples = G(z).data.cpu().numpy()[:16]
+			fig = plt.figure(figsize=(4, 4))
+			gs = gridspec.GridSpec(4, 4)
+			gs.update(wspace=0.05, hspace=0.05)
 
-			test_out = G(test_in)
-			test_out = test_out.data.numpy()
-			test_out = test_out.reshape(28,28)
-			test_out -= test_out.min()
-			test_out = np.uint8(255*test_out/test_out.max())
-			# save the output image
-			Image.fromarray(test_out, 'L').save(save_path+'test/%d.png'%epoch_idx,'png')
+			for i, sample in enumerate(samples):
+				ax = plt.subplot(gs[i])
+				plt.axis('off')
+				ax.set_xticklabels([])
+				ax.set_yticklabels([])
+				ax.set_aspect('equal')
+				plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
 
+			if not os.path.exists('out/'):
+				os.makedirs('out/')
+
+			plt.savefig('out/{}.png'.format(str(count).zfill(3)), bbox_inches='tight')
+			count += 1
+			plt.close(fig)
 
 
 if __name__ == "__main__":
-	train(10, 64, False)
+	train(10000, 64, torch.cuda.is_available())
+
+
+
+
+
+
+
+
+			
+
+			
--- GAN/wgan.py
+++ GAN/wgan.py
@@ -11,8 +11,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.nn.init import xavier_normal
 from torch.autograd import Variable 
-from torchvision import datasets, transforms
-from PIL import Image
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from tensorflow.examples.tutorials.mnist import input_data
 
 class Generator(nn.Module):
 	def __init__(self, z_dim, h_dim, x_dim):
@@ -48,7 +51,7 @@ def init_weights(layer):
 	if isinstance(layer, nn.Linear):
 		xavier_normal(layer.weight.data)
 
-z_dim = 100
+z_dim = 10
 x_dim = 28*28
 h_dim = 128
 
@@ -60,39 +63,24 @@ def build_graph(gpu = False):
 	G.apply(init_weights)
 	D.apply(init_weights)
 
+	loss = nn.BCELoss()
+
 	if gpu:
 		G.cuda()
 		D.cuda()
 		loss.cuda()
 
-	optimizerG = optim.RMSprop(G.parameters(), lr = 5e-5)
-	optimizerD = optim.RMSprop(D.parameters(), lr = 5e-5)
+	optimizerG = optim.RMSprop(G.parameters(), lr = 1e-4)
+	optimizerD = optim.RMSprop(D.parameters(), lr = 1e-4)
 
-	return G, D, optimizerG, optimizerD
+	return G, D, loss, optimizerG, optimizerD
 
-def train(n_epoch, batch_size, gpu = False, save_path = './wgan/'):
-	# check path
-	if not os.path.exists(save_path+'G/'):
-		os.makedirs(save_path+'G/')
-	if not os.path.exists(save_path+'D/'):
-		os.makedirs(save_path+'D/')
-	if not os.path.exists(save_path+'test/'):
-		os.makedirs(save_path+'test/')
-
+def train(step, batch_size, gpu = False):
 	# load input dataset
-	mnist = datasets.MNIST('mnist/', train = True, download = True, 
-							transform = transforms.Compose([
-								transforms.ToTensor(), 
-								transforms.Normalize((0.1307,), (0.3801,))]))
-
-	train_loader = torch.utils.data.DataLoader(mnist, 
-												batch_size = batch_size, 
-												shuffle = True, num_workers = 4, 
-												pin_memory = gpu, 
-												drop_last = True)
+	mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 	# build model
-	G, D, optimizerG, optimizerD = build_graph(gpu = gpu)
+	G, D, loss, optimizerG, optimizerD = build_graph(gpu = gpu)
 
 
 	# testing input
@@ -104,53 +92,68 @@ def train(n_epoch, batch_size, gpu = False, save_path = './wgan/'):
 	ones_label = Variable(torch.ones(batch_size, 1))
 	zeros_label = Variable(torch.zeros(batch_size, 1))
 
-	for epoch_idx in range(n_epoch):
-		for batch_idx, (data, _) in enumerate(train_loader):
-			data.resize_(batch_size, x_dim)
+	if gpu:
+		ones_label=ones_label.cuda()
+		zeros_label=zeros_label.cuda()
+	count = 0
+
+	for step_idx in range(step):
+		for _ in range(5):
+			data, _ = mnist.train.next_batch(batch_size)
+			data = torch.from_numpy(data)
+			noise = torch.randn(batch_size, z_dim)
 			if gpu:
-				data.cuda()
+				noise = noise.cuda()
+				data = data.cuda()
+			z = Variable(noise)
 			x = Variable(data)
-			for _ in range(5):
-				noise = torch.randn(batch_size, z_dim)
-				if gpu:
-					noise.cuda()
-				z = Variable(noise)
-				g_sample = G(z)
-				real = D(x)
-				fake = D(g_sample.detach())
+			g_sample = G(z)
+			real = D(x)
+			fake = D(g_sample.detach())
+			# train the discriminator --minimize L(D) = -[f(D(x)) - f(D(G(z)))]
+			D.zero_grad()
+			D_loss = -(torch.mean(real) - torch.mean(fake))
+			D_loss.backward()
+			optimizerD.step()
+			# weight clipping
+			for layer in D.parameters():
+				layer.data.clamp_(-0.01, 0.01)
 
-				# train the discriminator --minimize L(D) = -[f(D(x)) + f1-D(G(z)))]
-				D.zero_grad()
-				D_loss = -(torch.mean(real) - torch.mean(fake))
-				D_loss.backward()
-				optimizerD.step()
-				# weight clipping
-				for layer in D.parameters():
-					layer.data.clamp_(-0.01, 0.01)
-
-			# train the generator -- minimize L(G) = -f(D(G(z)))
-			G.zero_grad()
-			out = D(g_sample)
-			G_loss = -torch.mean(out)
-			G_loss.backward()
-			optimizerG.step()
-
-			print "Epoch: \033[95m%d\033[0m batch: \033[95m%d\033[0m" % (epoch_idx, batch_idx)
+		noise = torch.randn(batch_size, z_dim)
+		if gpu:
+			noise = noise.cuda()
+		z = Variable(noise)
+		g_sample = G(z)
+		# train the generator -- minimize L(G) = -f(D(G(z)))
+		G.zero_grad()
+		out = D(g_sample)
+		G_loss = -torch.mean(out)
+		G_loss.backward()
+		optimizerG.step()
 
 		# save the model weights and the testing sample
-		if epoch_idx%1 == 0:
-			torch.save(G.state_dict(), save_path+'G/modelG_%d.pth'%epoch_idx)
-			torch.save(D.state_dict(), save_path+'D/modelD_%d.pth'%epoch_idx)
+		if step_idx % 1000 == 0:
+			print 'Iter-{}; Wasserstein_loss: {:.4};'.format(step_idx, -D_loss.data[0])
+			samples = G(z).data.cpu().numpy()[:16]
+			fig = plt.figure(figsize=(4, 4))
+			gs = gridspec.GridSpec(4, 4)
+			gs.update(wspace=0.05, hspace=0.05)
 
-			test_out = G(test_in)
-			test_out = test_out.data.numpy()
-			test_out = test_out.reshape(28,28)
-			test_out -= test_out.min()
-			test_out = np.uint8(255*test_out/test_out.max())
-			# save the output image
-			Image.fromarray(test_out, 'L').save(save_path+'test/%d.png'%epoch_idx,'png')
+			for i, sample in enumerate(samples):
+				ax = plt.subplot(gs[i])
+				plt.axis('off')
+				ax.set_xticklabels([])
+				ax.set_yticklabels([])
+				ax.set_aspect('equal')
+				plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
 
+			if not os.path.exists('out/'):
+				os.makedirs('out/')
+
+			plt.savefig('out/{}.png'.format(str(count).zfill(3)), bbox_inches='tight')
+			count += 1
+			plt.close(fig)
 
 
 if __name__ == "__main__":
-	train(10, 64, False)
+	train(10000, 64, torch.cuda.is_available())
